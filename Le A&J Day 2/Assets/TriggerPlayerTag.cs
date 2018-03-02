6488ceb9d3e58257e73ebfd9ef787840d43c3537
Unknown <oncheonche@ymail.com>
2018-03-02 11:58:15 +0100

    Joueur bouge + looping (pre-alpha)
    ISSSSOUUUUU

---
+++ Le A&J Day 2/Assets/TriggerPlayerTag.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class TriggerPlayerTag : MonoBehaviour {
+
+    public string oldPlayerLayer;
+    public string newPlayerLayer;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (LayerMask.LayerToName(other.transform.gameObject.layer) == oldPlayerLayer)
+        {
+            other.transform.gameObject.layer = LayerMask.NameToLayer(newPlayerLayer);
+        }
+    }
+
+}
