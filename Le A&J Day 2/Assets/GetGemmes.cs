4b984e405e1591f856b7e800484abd3fe5e6ea01
Unknown <oncheonche@ymail.com>
2018-03-02 15:04:41 +0100

    DANS LA JUNGLE TERRIBLE JUNGLE

---
+++ Le A&J Day 2/Assets/GetGemmes.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetGemmes : MonoBehaviour {
+
+    public int Gemmes;
+    public string gemmesLayer;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (LayerMask.LayerToName(other.transform.gameObject.layer) == gemmesLayer)
+        {
+            Gemmes++;
+            Destroy(other.transform.gameObject);
+        }
+    }
+
+}
