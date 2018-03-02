b609b19784685db6b45ce0e9ad4a29c5a3d4a2ac
JulienGuenard <julienguenard01@gmail.com>
2018-03-02 15:25:03 +0100

    prout

---
+++ Le A&J Day 2/Assets/GemManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GemManager : MonoBehaviour {
+
+  public List<GameObject> listGem;
+    public static GemManager Instance;
+
+    // Use this for initialization
+    void Awake () {
+        Instance = this;
+
+        listGem.AddRange(GameObject.FindGameObjectsWithTag("gem"));
+    }
+}
